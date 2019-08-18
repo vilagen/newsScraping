fd70bab9c29fced46f8dfad0c100e272cadbbeb6
vilagen <vilagen@yahoo.com>
2019-08-18 18:05:27 -0400

    Set up routing for basic index page, set up express handlebars pages.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var mongoose = require("mongoose");
 var cheerio = require("cheerio")
 var mongojs = require("mongojs");
 var axios = require("axios");
+var exphbs = require("express-handlebars")
+var routes = require("./routes/routes")
 
 var logger = require("morgan");
 
@@ -17,7 +19,11 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+
+app.use(routes)
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
 
