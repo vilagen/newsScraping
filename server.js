8cc8e021ad7e7d1b85748b983ab6549a0a8b2b94
vilagen <vilagen@yahoo.com>
2019-08-19 00:12:45 -0400

    updating how results come into index page.

--- server.js
+++ server.js
@@ -59,10 +59,8 @@ app.get("/scrape", function(req, res) {
 });
 
 app.get("/articles", function(req, res) {
-    // Grab every document in the Articles collection
     db.Article.find({})
       .then(function(dbArticle) {
-        // If we were able to successfully find Articles, send them back to the client
         res.json(dbArticle);
       })
       .catch(function(err) {
@@ -70,6 +68,30 @@ app.get("/articles", function(req, res) {
         res.json(err);
       });
 });
+
+app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+      .populate("note")
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+  
+  app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
 
 app.listen(PORT, function() {
     console.log("App running on port " + PORT)
